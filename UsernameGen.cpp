1e086db2bc7b2afb6a1885d6c0b7dcab318674e9
Dashhhhhhhh <110487865+Dashhhhhhhh@users.noreply.github.com>
2022-08-22 00:05:56 -0400

    more results and comments

--- UsernameGen.cpp
+++ UsernameGen.cpp
@@ -11,16 +11,18 @@ int main(){
         restart = false;
         srand(time(NULL));
         int num;
-        int Ran1 = rand() % 10;
-        int Ran2 = rand() % 10;
-
-        std::array<std::string, 10> Part1 = { std::string("Kled"), "Captain", "Schlong", "Fiddle", "Loli", "Dong","Dingle","Slimy","Slow","Master"};
-        std::array<std::string, 10> Part2 = { std::string("Gecko"), "Main", "Camper", "Licous", "Moist", "Only","Smasher","Tingler","Sticky","Smurf"};
+       //randomization
+        int Ran1 = rand() % 15;
+        int Ran2 = rand() % 15;
+        // array of words
+        std::array<std::string, 15> Part1 = { std::string("Kled"), "Captain", "Schlong", "Fiddle", "Loli", "Dong","Dingle","Slimy","Slow","Master","Balls","Mega","Nutz","Faze","Sticky"};
+        std::array<std::string, 15> Part2 = { std::string("Gecko"), "Main", "Camper", "Licous", "Moist", "Only","Smasher","Tingler","Sticky","Smurf","Sticky","Booty","Buster","Pucket","Women"};
 
         num = rand() % 1000;
 
         std::cout << Part1[Ran1] << Part2[Ran2] << num;
         std::string in;
+        // restart func
         getline(std::cin, in);
         restart = true;
         Sleep(700);
