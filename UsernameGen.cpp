e271724962df26a194d4bee3107ec920a142bcfa
Dashhhhhhhh <110487865+Dashhhhhhhh@users.noreply.github.com>
2022-08-21 23:55:18 -0400

    UsernameGen fix
    Randomization fix

--- UsernameGen.cpp
+++ UsernameGen.cpp
@@ -11,14 +11,15 @@ int main(){
         restart = false;
         srand(time(NULL));
         int num;
-        int RanIndex = rand() % 10;
+        int Ran1 = rand() % 10;
+        int Ran2 = rand() % 10;
+
         std::array<std::string, 10> Part1 = { std::string("Kled"), "Captain", "Schlong", "Fiddle", "Loli", "Dong","Dingle","Slimy","Slow","Master"};
         std::array<std::string, 10> Part2 = { std::string("Gecko"), "Main", "Camper", "Licous", "Moist", "Only","Smasher","Tingler","Sticky","Smurf"};
 
-        
         num = rand() % 1000;
 
-        std::cout << Part1[RanIndex] << Part2[RanIndex] << num;
+        std::cout << Part1[Ran1] << Part2[Ran2] << num;
         std::string in;
         getline(std::cin, in);
         restart = true;
